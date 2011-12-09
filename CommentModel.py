73933d311233aaac5c7a3fa81435a652e966b1c2
Jeremy Hoyland <jeremy.hoyland@newsint.co.uk>
2011-12-09 15:04:35 +0000

    Now with reverse dictionaries

--- CommentModel.py
+++ CommentModel.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 from mongo_connection import MongoConnection
+from array import array
 import operator
 
 class CommentModel(MongoConnection) :
@@ -15,21 +16,43 @@ import string
 class Indexer :
 
   def __init__(self):
-    self.dictonary = {}
+    """Words and stats"""
+    self.dictonary = {} 
+    self.reverseWord = {} 
     self.sortedDict = {}
+    """articles to words"""
+    self.reverseDict = {}
+    """words to articles"""
+    self.forwardDict = {}
+    self.lastWord = 0
 
-  def index(self, str):
+  def index(self, comment):
     from stopList import stop_list
 
-    for word in self.preprocess(str):
+    article_words = array('i')
+    for word in self.preprocess(comment['content']):
       if word in stop_list :
         continue
-      self.dictonary[word] = self.dictonary.get(word,0) + 1
+      if  word in self.dictonary:
+        self.dictonary[word]['count'] = self.dictonary[word]['count'] + 1
+        #This won't be unique probably should be
+        self.forwardDict[self.dictonary[word]['index']].append(comment['id'])
+      else :
+        self.dictonary[word] = {'index': self.lastWord, 'count': 1}
+        self.forwardDict[self.lastWord] = array('i')
+        self.forwardDict[self.lastWord].append(comment['id'])
+        self.reverseWord[self.lastWord] = word
+        self.lastWord = self.lastWord + 1
+
+      article_words.append(self.dictonary[word]['index'])
+
+    self.reverseDict[comment['id']] = article_words;
 
   def sort(self):
     self.sortedDict = sorted(self.dictonary.iteritems(), key=operator.itemgetter(1), reverse=True)
 
   def get(self, limit=10):
+    #print repr(self.dictonary)
     if limit > 0:
       return self.sortedDict[0:limit]
     return self.sortedDict
@@ -40,7 +63,9 @@ class Indexer :
       s = str(s.lower())
       s = s.translate(None, string.punctuation)
     except UnicodeEncodeError  as msg : 
-      print 'Unicode Error : ', msg
+      X = 1
+      #print 'Unicode Error : ', msg
+
     words = s.split()
     return words
 
@@ -48,8 +73,10 @@ if __name__ == "__main__":
   c = CommentModel()
   indexer = Indexer()
 
-  for comment in c.getAllComments(0) :
-    indexer.index(comment['content'])
+  for comment in c.getAllComments(100) :
+    indexer.index(comment)
 
   indexer.sort()
-  print indexer.get()
+  print str(comment['id'])  + ' : ' +  comment['content'] + '\n'
+  for i in indexer.reverseDict[comment['id']]:
+    print indexer.reverseWord[i] + ' '
