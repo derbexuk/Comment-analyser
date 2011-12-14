515d1b3bb1d1f9035466f881ecdc3120c9682870
Jeremy Hoyland <jeremy.hoyland@newsint.co.uk>
2011-12-14 17:51:50 +0000

    starting on shredder recomposition

--- ai-nlp/shredded.py
+++ ai-nlp/shredded.py
@@ -1,4 +1,5 @@
 from array import array
+from Trigrams import Trigram
 
 f = open('shredded.txt')
 
@@ -8,7 +9,21 @@ for str in  f:
 
 """Transpose"""
 col_table = zip(*input_table)
-print repr(col_table[1][5])
+max_i = 0;
+max_prob = -1000000
+for i in range(0,len(col_table)):
+#for i in range(0,3):
+  prob = 1
+  for j in range(0,len(col_table[i])):
+    word = " " + col_table[i][j]
+    prob = prob + Trigram.tgram_prob(word.upper())
+  print "%s : %f" % (word, prob)
+  if prob > max_prob:
+    max_prob = prob
+    max_i = i
+
+print "Col %d Prob: %f" % (max_i, max_prob)
+print repr(col_table[max_i][j])
 #for line in input_table:
   #print line[2]
 
